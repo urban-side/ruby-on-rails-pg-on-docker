71edf3a93d4a92717aabb57ebd005cbfbd2a0d74
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-08-01 12:54:42 +0900

    form builderで作成フォームの実装

--- app/controllers/summary_detail_controller.rb
+++ app/controllers/summary_detail_controller.rb
@@ -1,4 +1,5 @@
 class SummaryDetailController < ApplicationController
+  
   def index
     @tasks = Task.all
   end
@@ -8,4 +9,27 @@ class SummaryDetailController < ApplicationController
     @status = ["未着手", "進行中", "完了"]
     @priority_mark = ["", "!", "!!", "!!!"]
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      title: "",
+      description: "",
+      status: 0,
+      date: "", 
+      priority: 0,
+      label: 0, 
+      user: 0
+    )
+
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end

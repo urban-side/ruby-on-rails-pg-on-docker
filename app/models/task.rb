5b3ab92670f594bf9c7b3d0df3b4c34947d0dc5c
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-08-08 18:38:08 +0900

    フォームに対して設計当時の情報を入力できるように項目を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
-    validates :title, presence: true
+    validates :title,       presence: true
     validates :description, presence: true
+    validates :status,      presence: true
+    
+    # 初期値設定（新規作成時のみなので if~ は必須）
+    after_initialize :set_default, if: :new_record?
+    private
+    def set_default
+        self.status = 0
+    end
 end

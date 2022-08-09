9d003adf95ef1a42cb863b065fc77f1f3611795f
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-08-09 11:40:12 +0900

    fix: 型変更処理はロールバック用にup/downメソッドで書き換え

--- db/migrate/20220808093954_change_datatype_status_of_tasks.rb
+++ db/migrate/20220808093954_change_datatype_status_of_tasks.rb
@@ -1,7 +1,11 @@
 class ChangeDatatypeStatusOfTasks < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tasks, :status, :integer, limit: 1
-    change_column_null :tasks, :status, false
-    change_column :tasks, :priority, :integer, limit: 1
+  def up
+    change_column :tasks, :status, :smallint, null: false
+    change_column :tasks, :priority, :smallint
+  end
+
+  def down
+    change_column :tasks, :status, :integer, null: true
+    change_column :tasks, :priority, :integer
   end
 end

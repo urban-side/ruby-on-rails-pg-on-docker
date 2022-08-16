20b88c7068c368e6f9a9a5bbb852e561a7155d3e
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-08-16 10:37:59 +0900

    generate FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Pythonでいう"from Facrotybot ipmort *" 的な意味になる
+  config.include Factorybot::Syntax::Methods
 end

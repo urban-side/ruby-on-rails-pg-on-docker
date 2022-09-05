2906cfc8d213f7e008ceec9e059f968b0785cbc8
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-09-05 11:31:02 +0900

    done sort test with pry_rails

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
 
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'pry-byebug'
 end
 
 group :development do
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -4,16 +4,46 @@ RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
     before do
       # FactoryBotで登録
-      create(:task)
+      @task0 = create(:task, title: "aaa", date: "2022-01-01 10:00:00", priority: 3, created_at: "2010-01-01 10:00:00")
+      @task1 = create(:task, title: "テスト", date: "2022-02-02 10:00:00", priority: 1, created_at: "2000-01-01 10:00:00")
+      @task2 = create(:task, title: "XXX", date: "2022-03-03 10:00:00", priority: 2, created_at: "2020-01-01 10:00:00")
     end
     
-    it do
+    it "success the access" do
       get root_path
-      # 登録したデータとここで作ったActiveRecordインスタンスが同じならOK
-      # !!! ：細かい参照方法（配列だから要素で指定、key-value関係の可能性など）が不明なんで、おそらくエラー出ると思います
+      expect(response).to be_successful
+    end
+    
+    it "sorted by title" do
+      get root_path, params: {direction: 'asc', sort: 'title'}
+      # binding.pry
       expect(
-        controller.instance_variable_get('@user')
-      ).to eq build(:task)
+        controller.instance_variable_get('@tasks')
+      ).to eq [@task2, @task0, @task1]
+    end
+
+    it "sorted by created-at date" do
+      get root_path, params: {direction: 'asc', sort: 'created_at'}
+      # binding.pry
+      expect(
+        controller.instance_variable_get('@tasks')
+      ).to eq [@task1, @task0, @task2]
+    end
+
+    it "sorted by deadline date" do
+      get root_path, params: {direction: 'asc', sort: 'date'}
+      # binding.pry
+      expect(
+        controller.instance_variable_get('@tasks')
+      ).to eq [@task0, @task1, @task2]
+    end
+
+    it "sorted by priority" do
+      get root_path, params: {direction: 'asc', sort: 'priority'}
+      # binding.pry
+      expect(
+        controller.instance_variable_get('@tasks')
+      ).to eq [@task1, @task2, @task0]
     end
   end
 end

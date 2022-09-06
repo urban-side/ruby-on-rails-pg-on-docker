7bd8d23b6b0cafa71b98e16dd24987d322979545
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-09-06 18:03:37 +0900

    余計な表示項目と軽微な初期値バグの修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,11 @@ class Task < ApplicationRecord
     validates :description, presence: true
     validates :status,      presence: true
     
-    # 初期値設定（新規作成時のみなので if~ は必須）
-    after_initialize :set_default, if: :new_record?
-    private
-    def set_default
-        self.status = 0
-    end
+    # 20220906追記：初期値が保存時まで効いてしまう（必須項目エラーは出すのでそちらで対応）
+    # # 初期値設定（新規作成時のみなので if~ は必須）
+    # after_initialize :set_default, if: :new_record?
+    # private
+    # def set_default
+    #     self.status = 0
+    # end
 end

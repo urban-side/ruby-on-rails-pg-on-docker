721f003c997f296dab28c3984de411aeed1cbf7c
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-09-12 16:22:41 +0900

    init users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_09_020945) do
+ActiveRecord::Schema.define(version: 2022_09_12_053359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2022_08_09_020945) do
     t.integer "priority", limit: 2
     t.integer "label"
     t.text "user"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

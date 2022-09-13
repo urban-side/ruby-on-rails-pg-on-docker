d2638d0116c8466b1c8119c43f7db5ee83a93c0b
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-09-13 19:03:21 +0900

    create login/logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root "tasks#index"
   resources :tasks do
     collection do
       get  "search"
-    end    
+    end
   end
 
+  # resourcesを使うフルセットルーティングはいらないので個別指定
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end

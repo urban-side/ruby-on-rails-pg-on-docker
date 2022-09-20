a78d6fcc5dd0db2b8881767506a33903ac98a1be
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-09-20 19:08:46 +0900

    adjust controller and routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,77 @@
 class UsersController < ApplicationController
-  def new
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
   end
+  
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to this App!"
+      redirect_to root_url
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "User was successfully updated"
+        redirect_to @user
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @user = User.find(session[:user_id])
+    if @user.destroy
+      flash[:success] = 'User was successfully deleted.'
+      redirect_to users_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to users_url
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   root "tasks#index"
+
   resources :tasks do
     collection do
       get  "search"
     end
   end
+
+  resources :users
 
   # resourcesを使うフルセットルーティングはいらないので個別指定
   get    '/login',   to: 'sessions#new'

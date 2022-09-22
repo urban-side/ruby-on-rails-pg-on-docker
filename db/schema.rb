67e4bc27b5b1f66eaa4f1c37a677cc1925a0e48a
h.kanazawa <h.kanazawa@buysell-technologies.com>
2022-09-22 15:28:29 +0900

    add role colunm in users and adjust seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_073151) do
+ActiveRecord::Schema.define(version: 2022_09_22_055212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_09_13_073151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
